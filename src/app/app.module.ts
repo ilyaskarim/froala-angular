c0563fd9fc03e62aff0f1e26ac6fa8e5ee9374c4
ilyaskarim <iksdatoo@gmail.com>
2019-10-27 10:44:09 -0700

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+
+import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
+
+
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, FroalaEditorModule.forRoot(), FroalaViewModule.forRoot() ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
